422b5c18f4e5216c5280b856524a1d1e1df89082
aprguez <alejandro_p-rguez@hotmail.com>
2021-07-09 03:28:33 +0200

    Flechas final

--- bitmap.asm
+++ bitmap.asm
@@ -25,6 +25,33 @@ memoria:
 	sw x10, 0x10C(t0)
 	sw x10, 0x110(t0)
 	
+	#--Superior derecha
+	sw x10, 0x14(t0)
+	sw x10, 0x18(t0)
+	sw x10, 0x1C(t0)
+	sw x10, 0x58(t0)
+	sw x10, 0x5C(t0)
+	sw x10, 0x94(t0)
+	sw x10, 0x9C(t0)
+	
+	#--Inferior izquierda
+	sw x10, 0x140(t0)
+	sw x10, 0x148(t0)
+	sw x10, 0x180(t0)
+	sw x10, 0x184(t0)
+	sw x10, 0x1C4(t0)
+	sw x10, 0x1C0(t0)
+	sw x10, 0x1C8(t0)
+	
+	#--Inferior derecha
+	sw x10, 0x154(t0)
+	sw x10, 0x15C(t0)
+	sw x10, 0x198(t0)
+	sw x10, 0x19C(t0)
+	sw x10, 0x1D4(t0)
+	sw x10, 0x1D8(t0)
+	sw x10, 0x1DC(t0)
+	
 	#-- Terminar
 	li a7, 10
 	ecall
